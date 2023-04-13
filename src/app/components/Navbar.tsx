c53e91b57aa65c98fbfc2c9f07d13f8bbba26772
fdemir <frndemir@gmail.com>
2023-04-13 20:17:11 +0300

    fix: remove old dummy dialog

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -17,32 +17,11 @@ const Navbar = () => {
           <button onClick={() => signIn("github")}>Sign in with Github</button>
         ) : (
           <div>
-            <Dialog.Root>
-              <Dialog.Trigger asChild>
-                <button>Add</button>
-              </Dialog.Trigger>
-              <Dialog.Portal>
-                <Dialog.Overlay className="bg-black bg-opacity-25 data-[state=open]:animate-overlayShow fixed inset-0" />
-                <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-                  <Dialog.Title className="text-black m-0 text-[17px] font-medium">
-                    Add
-                  </Dialog.Title>
-                  <Dialog.Description className="text-gray-500 mt-[10px] mb-5 text-[15px] leading-normal">
-                    Choose your github repository to add.
-                    <i>It must be public.</i>
-                  </Dialog.Description>
-                  <Dialog.Close asChild>
-                    <button
-                      className="text-slate-500 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full "
-                      aria-label="Close"
-                    >
-                      <Cross2Icon />
-                    </button>
-                  </Dialog.Close>
-                </Dialog.Content>
-              </Dialog.Portal>
-            </Dialog.Root>
             <div className="flex gap-3">
+              <button>
+                Add
+                {/* TODO: use radix dialog */}
+              </button>
               <button onClick={() => signOut()}>Sign out</button>
             </div>
           </div>
