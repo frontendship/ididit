48be9ec0402500bd8b3a5d7adf01772bca532007
Furkan Demir <48316033+fdemir@users.noreply.github.com>
2023-04-15 02:30:02 +0300

    Feat/add (#2)
    * update
    
    * fix: remove old dummy dialog

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
 const Navbar = () => {
   const session = useSession();
 
   return (
-    <div className="h-20  items-center flex justify-between px-8">
+    <nav className="h-20  items-center flex justify-between md:px-0 px-8">
       <div>
         <h1 className="text-2xl font-bold">I did it🏳️‍🌈</h1>
       </div>
@@ -14,10 +16,18 @@ const Navbar = () => {
         {session.status === "unauthenticated" ? (
           <button onClick={() => signIn("github")}>Sign in with Github</button>
         ) : (
-          <button onClick={() => signOut()}>Sign out</button>
+          <div>
+            <div className="flex gap-3">
+              <button>
+                Add
+                {/* TODO: use radix dialog */}
+              </button>
+              <button onClick={() => signOut()}>Sign out</button>
+            </div>
+          </div>
         )}
       </div>
-    </div>
+    </nav>
   );
 };
 
