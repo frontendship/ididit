6409c9aa8b89ff224f7e7574f1cb801e0523d0d2
fdemir <frndemir@gmail.com>
2023-04-14 16:23:57 +0300

    feat: add dialog component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
-import * as Dialog from "@radix-ui/react-dialog";
-import { Cross2Icon } from "@radix-ui/react-icons";
+import AddDialog from "./AddDialog";
 
 const Navbar = () => {
   const session = useSession();
 
   return (
-    <nav className="h-20  items-center flex justify-between md:px-0 px-8">
+    <nav className="flex items-center justify-between h-20 px-8 md:px-0">
       <div>
         <h1 className="text-2xl font-bold">I did it🏳️‍🌈</h1>
       </div>
@@ -18,10 +17,9 @@ const Navbar = () => {
         ) : (
           <div>
             <div className="flex gap-3">
-              <button>
-                Add
-                {/* TODO: use radix dialog */}
-              </button>
+              <AddDialog>
+                <button>Add</button>
+              </AddDialog>
               <button onClick={() => signOut()}>Sign out</button>
             </div>
           </div>
