560b9bd199db3fa267d5721a7240587d9173299b
fdemir <frndemir@gmail.com>
2023-04-16 23:43:42 +0300

    refactor: update nextuath

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,8 +1,13 @@
+import type { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import type { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       // FIXME: This is a hack to get around env variables typings. Need to fix this.
