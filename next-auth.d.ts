4f16b874a716d1a874e3f00acd24a743413aaf49
Furkan Demir <48316033+fdemir@users.noreply.github.com>
2023-05-10 09:01:41 +0300

    Update session object (#19)
    * docs: extend session object type
    
    * feat: save users username into session object
    
    changed strategy to jwt since it's flex

---
+++ next-auth.d.ts
@@ -0,0 +1,13 @@
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    profile: {
+      username: string;
+    };
+    user: DefaultSession["user"];
+  }
+}
--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,4 +1,4 @@
-import type { NextAuthOptions } from "next-auth";
+import type { NextAuthOptions, Session } from "next-auth";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -16,6 +16,26 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, profile, account }) {
+      // TODO: Maybe we will add more providers in the future. For now, we only have github. This logic could be mapped to a switch etc. statement.
+      if (profile && account?.provider === "github") {
+        token["profile"] = {
+          // @ts-ignore
+          username: profile?.login,
+        };
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      let userData = token.profile as Session["profile"];
+      session.profile = userData;
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
 };
 
 export default NextAuth(authOptions);
